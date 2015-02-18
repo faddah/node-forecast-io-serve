49b2ae83b9ca1a932f327988156a47708e3cab4d
Adam Ulvi <adam@pokemedia.com>
2015-02-18 12:29:17 -0800

    More cleanup. Ready for hackin'

--- forecast.js
+++ forecast.js
@@ -8,7 +8,7 @@ zipcodes = ["10011", "20050", "91308", "90069", "94101", "97215"];
 // Loop over each zipcode, one after another.
 async.eachSeries(zipcodes, function (zipcode, next) {
 	// Do something w/the zipcode.
-	zip-forecast.getForecast(zipcode);
+	zip.getForecast(zipcode);
 	next(null);
 });
 
